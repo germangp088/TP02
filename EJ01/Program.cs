4207eb2a4732e00a82cbfbd4e813539252536e7f
germangp088 <germangp088@gmail.com>
2017-09-09 22:30:59 -0300

    Subo proyecto.

---
+++ EJ01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EJ01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cuadrado cuadrado = new Cuadrado(4.8);
+            Console.WriteLine("Perimero del cuadrado:" + cuadrado.CalcularPerimetro());
+            Console.WriteLine("Area del cuadrado:" + cuadrado.CalcularArea());
+
+            Circulo circulo = new Circulo(6.7);
+            Console.WriteLine("Perimero del circulo:" + circulo.CalcularPerimetro());
+            Console.WriteLine("Area del circulo:" + circulo.CalcularArea());
+
+            Triangulo triangulo = new Triangulo(6, 7, 10, 10);
+            Console.WriteLine("Perimero del triangulo:" + triangulo.CalcularPerimetro());
+            Console.WriteLine("Area del triangulo:" + triangulo.CalcularArea());
+            Console.Read();
+        }
+    }
+}
---
+++ EJ01/Triangulo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJ01
+{
+    class Triangulo : FiguraGeometrica
+    {
+        List<double> lados = new List<double>();
+        double altura;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="cateto1">Base del triangulo</param>
+        /// <param name="cateto2"></param>
+        /// <param name="hipotenuza"></param>
+        /// <param name="altura"></param>
+        public Triangulo(double cateto1, double cateto2, double hipotenuza, double altura)
+        {
+            lados.Add(cateto1);
+            lados.Add(cateto2);
+            lados.Add(hipotenuza);
+            this.altura = altura;
+        }
+        public double CalcularArea()
+        {
+            return (lados[0] * altura) / 2;
+        }
+
+        public double CalcularPerimetro()
+        {
+            double resultado = 0;
+            foreach (var lado in lados)
+            {
+                resultado += lado;
+            }
+            return resultado;
+        }
+    }
+}
